e4c725810c8ede8b463e86a61c7285ccd0b076fe
Muhammed Roshan <roshantac@gmail.com>
2023-06-09 09:38:49 +0530

    added. files

---
+++ Session6/utils.py
@@ -0,0 +1,33 @@
+from tqdm import tqdm
+import torch.nn.functional as F
+import torch
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    pbar = tqdm(train_loader)
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        pbar.set_description(desc= f'loss={loss.item()} batch_id={batch_idx}')
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
